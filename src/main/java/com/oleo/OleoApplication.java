05c6d5ababfc749a373cfdf9a39c443bc12aca5e
Anderson Scarranaro <ascarranaro@minsait.com>
2024-01-29 14:44:29 -0300

    Beggin project

---
+++ src/main/java/com/oleo/OleoApplication.java
@@ -0,0 +1,13 @@
+package com.oleo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OleoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OleoApplication.class, args);
+	}
+
+}
